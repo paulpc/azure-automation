58447a3a62e61e517ca9a8a3dcc767add8bec8c2
Paul PC <poputp@Pauls-Hostile-Workstation.local>
2018-02-06 21:35:20 -0600

    relogger remove and recreate

--- relogger.ps1
+++ relogger.ps1
@@ -2,15 +2,10 @@ $eventHubString = get-content eventhub.txt
 foreach ($subby in Get-AzureRmSubscription ) {
     Write-Host $subby.SubscriptionName
     Select-AzureRmSubscription -Subscription $subby.id
-    $logging=$FALSE
     foreach ($logpro in Get-AzureRmLogProfile) {
-        Write-Host $logpro
-        #.ServiceBusRuleId, $logpro.Name
-        $logging=$TRUE
-
+        # powershell sucks - so let's remove the existing logging profile
+        Write-Host "Remove-AzureRmLogProfile -Name $logpro.Name"
     }
-    if (! $logging) {
-        # creating the log profile here
-        Write-Host 'Add-AzureRmLogProfile -Name default -ServiceBusRuleId $eventHubString -RetentionInDays 0 -Location "centralus,northcentralus,southcentralus,westus,eastus,eastus2,global"'
-    }
+    # creating the log profile here
+    Write-Host "Add-AzureRmLogProfile -Name default -ServiceBusRuleId $eventHubString -RetentionInDays 0 -Location 'centralus,northcentralus,southcentralus,westus,eastus,eastus2,global'"
 }
