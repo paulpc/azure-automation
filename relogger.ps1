3de59599ae12598477defddf5597dd4f16ed839d
Paul PC <poputp@Pauls-Hostile-Workstation.local>
2018-02-06 20:58:17 -0600

    rlg

--- relogger.ps1
+++ relogger.ps1
@@ -2,12 +2,12 @@ $eventHubString = get-content eventhub.txt
 foreach ($subscription in Get-AzureRmSubscription ) {
     Write-Host $subscription.SubscriptionName
     Select-AzureRmSubscription -Subscription $subscription.id
-    $logging=false
+    $logging=$FALSE
     foreach ($logpro in Get-AzureRmLogProfile) {
         Write-Host $logpro.ServiceBusRuleId, $logpro.Name
-        $logging=True
+        $logging=$TRUE
     }
-    if (not $logging) {
+    if (! $logging) {
         # creating the log profile here
         Write-Host "this sub does not have a logging profile; we should create it"
         #Add-AzureRmLogProfile -Name default -ServiceBusRuleId $eventHubString -RetentionInDays 0 -Location "Centralus,northcentralus,southcentralus,westus,eastus,eastus2,global"
