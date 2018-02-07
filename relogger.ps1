ea4cbb97bad547a1ee655f381a83ae1c9eb59b73
Paul PC <poputp@Pauls-Hostile-Workstation.local>
2018-02-06 20:35:15 -0600

    relogger fix

--- relogger.ps1
+++ relogger.ps1
@@ -1,5 +1,5 @@
 foreach ($subscription in Get-AzureRmSubscription ) {
-    echo $subscription.subscriptionName
+    echo $subscription.SubscriptionName
     Select-AzureRmSubscription -Subscription $subscription.id
     foreach ($logpro in Get-AzureRmLogProfile) {
         echo $logpro.ServiceBusRuleId
