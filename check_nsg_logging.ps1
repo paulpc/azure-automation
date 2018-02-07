89902833fc620fb36cb83f2f217b7244f952af51
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2018-02-07 10:18:35 -0600

    dashes...

--- check_nsg_logging.ps1
+++ check_nsg_logging.ps1
@@ -2,7 +2,7 @@ foreach ($subby in Get-AzureRmSubscription ) {
     Select-AzureRmSubscription -Subscription $subby.id
     foreach ($nsg in Get-AzureRmNetworkSecurityGroup) {
         $loc=$nsg.Location
-        $networkwatcher = Get-AzurermNetworkWatcher -ResourceGroupName NetworkWatcherRg -Name "NetworkWatcher_$loc"
+        $networkwatcher = Get-AzurermNetworkWatcher -ResourceGroupName NetworkWatcherRg -Name "NetworkWatcher-$loc"
         #Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id
     }
 }
