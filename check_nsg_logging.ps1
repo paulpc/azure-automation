3331c235376bd1482c5d7cb3240852db4a626a3e
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2018-02-07 10:23:51 -0600

    network watcher

--- check_nsg_logging.ps1
+++ check_nsg_logging.ps1
@@ -1,8 +1,11 @@
 foreach ($subby in Get-AzureRmSubscription ) {
     Select-AzureRmSubscription -Subscription $subby.id
     foreach ($nsg in Get-AzureRmNetworkSecurityGroup) {
-        $loc=$nsg.Location
-        $networkwatcher = Get-AzurermNetworkWatcher -ResourceGroupName NetworkWatcherRg -Name "NetworkWatcher-$loc"
-        #Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id
+        foreach ($networkwatcher in Get-AzurermNetworkWatcher  -ResourceGroupName NetworkWatcherRg) {
+            if ($nsg.Location == $networkwatcher.Location) {
+                Write-Host $networkwatcher.Name
+            }
+
+        }        #Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id
     }
 }
