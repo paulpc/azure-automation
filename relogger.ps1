f8fa079a3a5e1ac2ea4ae234c9b385487550180b
Paul PC <poputp@Pauls-Hostile-Workstation.local>
2018-02-06 21:55:27 -0600

    rl

--- relogger.ps1
+++ relogger.ps1
@@ -4,6 +4,7 @@ foreach ($subby in Get-AzureRmSubscription ) {
     Select-AzureRmSubscription -Subscription $subby.id
     $logpro=Get-AzureRmLogProfile
     $logpro.Name
+    $logpro.location
     if ($logpro) {
         # powershell sucks - so let's remove the existing logging profile
         Write-Host "Remove-AzureRmLogProfile -Name" $logpro.Name
