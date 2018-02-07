b1db61c2c83258a6772dc30c5c3db59d0631d96c
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2018-02-07 16:26:40 -0600

    changing things around

--- check_nsg_logging.ps1
+++ check_nsg_logging.ps1
@@ -7,6 +7,21 @@ $tags["Build Date"]=get-date -UFormat "%m/%d/%Y"
 foreach ($subby in Get-AzureRmSubscription ) {
     Select-AzureRmSubscription -Subscription $subby.id
     foreach ($nsg in Get-AzureRmNetworkSecurityGroup) {
+        # creating the blobs where not already there
+        $found=$false
+        foreach ($store in Get-AzureRmStorageAccount -ResourceGroupName NetworkWatcherRG) {
+            # looking for blobs in the NetworkWatcherRG group
+            if ($store.Location -eq $nsg.Location) {
+                # that match the location of the NSG
+                $found=$true
+            }
+        }
+        # if the blobs don't exist, go ahead and make them
+        if ( -NOT ($found) ) {
+            Write-Host "Need to create storage $($subby.id.split("-")[0])$($nsg.Location) for $($nsg.Name) in $($nsg.Location)"
+            New-AzureRmStorageAccount -name "$($subby.id.split("-")[0])$($nsg.Location)" -Kind BlobStorage -Location $($nsg.Location) -SkuName Standard_LRS -ResourceGroupName NetworkWatcherRG -EnableHttpsTrafficOnly $true -AccessTier Hot -Tag $tags
+        }
+
         foreach ($networkwatcher in Get-AzurermNetworkWatcher  -ResourceGroupName NetworkWatcherRg) {
             if ($nsg.Location -eq $networkwatcher.Location) {
                 $flstatus = Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id
@@ -16,16 +31,7 @@ foreach ($subby in Get-AzureRmSubscription ) {
                 }
             }
         }
-        $found=$false
-        foreach ($store in Get-AzureRmStorageAccount -ResourceGroupName NetworkWatcherRG) {
-            if ($store.Location -eq $nsg.Location) {
-                $found=$true
-            }
-        }
-        if ( -NOT ($found) ) {
-            Write-Host "Need to create storage $($subby.id.split("-")[0])$($nsg.Location) for $($nsg.Name) in $($nsg.Location)"
-            New-AzureRmStorageAccount -name "$($subby.id.split("-")[0])$($nsg.Location)" -Kind BlobStorage -Location $($nsg.Location) -SkuName Standard_LRS -ResourceGroupName NetworkWatcherRG -EnableHttpsTrafficOnly $true -AccessTier Hot -Tag $tags
-        }
+        
 
     }
 }
