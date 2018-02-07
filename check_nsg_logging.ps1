c56193b6f3dd535af63670aab6104c5bdbfed858
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2018-02-07 10:45:50 -0600

    plus store

--- check_nsg_logging.ps1
+++ check_nsg_logging.ps1
@@ -3,8 +3,20 @@ foreach ($subby in Get-AzureRmSubscription ) {
     foreach ($nsg in Get-AzureRmNetworkSecurityGroup) {
         foreach ($networkwatcher in Get-AzurermNetworkWatcher  -ResourceGroupName NetworkWatcherRg) {
             if ($nsg.Location -eq $networkwatcher.Location) {
-                Write-Host $networkwatcher.Name, $nsg.Name
-                Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id
+                Write-Host $networkwatcher.Name, $nsg.Name, $subby.SubscriptionName
+                $flstatus = Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id
+                if (-NOT ($flstatus.Enabled)) {
+                    foreach ($store in Get-AzureRmStorageAccount -ResourceGroupName NetworkWatcherRG) {
+                        $found=$fasle
+                        if ($store.Location -eq $nsg.Location) {
+                            Write-Host "you should enable logging for $nsg.Name"
+                            $found=$true
+                        }
+                    }
+                    if ( -NOT $found) {
+                        Write-Host "Need to create storage for  $nsg.Name in  $nsg.Location "
+                    }
+                }
             }
         }
     }
