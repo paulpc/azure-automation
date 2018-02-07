89e0e293c41565588d3ebe581091c541e06e1f82
Paul PC <poputp@Pauls-Hostile-Workstation.local>
2018-02-06 21:41:01 -0600

    if statement

--- relogger.ps1
+++ relogger.ps1
@@ -3,8 +3,11 @@ foreach ($subby in Get-AzureRmSubscription ) {
     Write-Host $subby.SubscriptionName
     Select-AzureRmSubscription -Subscription $subby.id
     $logpro=Get-AzureRmLogProfile
-    # powershell sucks - so let's remove the existing logging profile
-    Write-Host "Remove-AzureRmLogProfile -Name $logpro.Name"
+    if ($logpro) {
+        # powershell sucks - so let's remove the existing logging profile
+        Write-Host "Remove-AzureRmLogProfile -Name $logpro.Name"
+    }
+
     # creating the log profile here
     Write-Host "Add-AzureRmLogProfile -Name default -ServiceBusRuleId $eventHubString -RetentionInDays 0 -Location 'centralus,northcentralus,southcentralus,westus,eastus,eastus2,global'"
 }
