97a8d0c2ecc914cc2bd59f748bbef6872aefbcb6
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2018-02-08 12:07:19 -0600

    apparenltly plural nouns are discouraged in this establishment

--- relogger.ps1
+++ relogger.ps1
@@ -11,5 +11,5 @@ foreach ($subby in Get-AzureRmSubscription ) {
         Remove-AzureRmLogProfile -Name $logpro.Name
     }
     # (re)creating the log profile here
-    Add-AzureRmLogProfile -Name default -ServiceBusRuleId $eventHubString -RetentionInDays 0 -Locations "Global","Central US","East US","East US 2","West US","North Central US","South Central US","West Central US","West US 2"
+    Add-AzureRmLogProfile -Name default -ServiceBusRuleId $eventHubString -RetentionInDays 0 -Location "Global","Central US","East US","East US 2","West US","North Central US","South Central US","West Central US","West US 2"
 }
