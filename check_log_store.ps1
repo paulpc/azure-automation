1a9e7fc0d84a40a3c94214360cc7d67c73545fdf
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2018-02-08 12:27:36 -0600

    meh

--- check_log_store.ps1
+++ check_log_store.ps1
@@ -2,8 +2,9 @@ foreach ($subby in Get-AzureRmSubscription ) {
     Write-Host "processing: " $subby.id $subby.Name
     Select-AzureRmSubscription $subby
     foreach ($store in Get-AzureRmStorageAccount -ResourceGroupName NetworkWatcherRG) {
+        $store
         if (-not ($store.Name -eq "$($subby.id.split("-")[0])$($store.Location)")) {
-            Write-Host $store
+            Write-Host "faulty creation. Fix me plz"
         }
     }
 }
